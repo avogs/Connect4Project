Completed FromScratchNN gradient descent algorithm and helper functions for that. Also fixed bugs and added documentation throughout. Added a useage example of FromScratchNN to Main.py.

Connect4AI/Connect4AI/FromScratchNN.py
@@ -3,7 +3,7 @@ import numpy as np
 class FromScratchNN(object):
     """A class which can be used to make and train a neural network. Built using minimal external libraries.
     
-    This is being built as a project to review/ dive deeper into python coding and deep learning internals."""
+    This is being built as a project to review/dive deeper into python coding and deep learning internals."""
 
     def __init__(self, layerSizes):
         """Initializes a list of layers representing a neural network.
@@ -11,9 +11,9 @@ class FromScratchNN(object):
         Goes from from an input layer of length layerSizes[0] to an
         output layer of length of layerSizes[len(layerSizes)]. These
         layers are initialized with 0 as all their parameters."""
-        self.layers = [] # A list of layer objects, ordered from input layer to output layer. Will always have at least 1 element.
+        self.layers = [] # A list of layer objects, ordered from input layer to output layer. Should always have at least 1 element.
         for x in range(1,len(layerSizes)):
-            self.layers[x-1] = layer(layerSizes[x-1], layerSizes[x])
+            self.layers.append(self.layer(layerSizes[x-1], layerSizes[x]))
 
 
     def predict(self, input):
@@ -24,53 +24,106 @@ class FromScratchNN(object):
         return nextOutput
 
 
-    #def trainGradientDescent(self, data, labels, gradientSpacing, learningRate):
-    #    """ """
-    #    # Note that this uses a numerical gradient which can give fairly bad results and will usually be highly time intensive.
-    #    # A more advanced method should use automatic differentiation or something similar but that is out of the scope of this project (for now).
-    #    np.gradient(self.parameterSpaceCostField(gradientSpacing))
-
-
-    #def parameterSpaceCostField(self, gradientSpacing, size):
-    #    """ """
-    #    numParams = 0
-    #    for layer in self.layer:
-    #        numParams += len(layer.biases)
-    #        numParams += len(layer.weights)
-    #    field = np.zeros((3, numParams))
-    #    
-    #    for layer in self.layers:
-    #        for bias in layer.bias:
-    #            bias += gradientSpacing
-    #            cost(data, labels)
-
+    def trainGradientDescent(self, data, labels, gradientSpacing, learningRate, epochs):
+        """A simple gradient descent implementation, takes data, labels, a spacing scalar for the numeric gradient evaluation, a learning rate scalar, and a number of iterations.
+        
+        A FromScratchNN object should first be made and given layer parameters, then this can be invoked and the parameters will be procedurally updated
+        for each epoch. Note that the numerical gradient used is time intensive and may yield subpar results on very small spacing due to rounding errors.
+        This uses a sum of squared errors cost function by default, others may be added later."""
+        for x in range(epochs):
+            # Print status at start of epoch
+            print("Beginning epoch: " + str(x))
+            print("Current cost: " + str(self.cost(data, labels)))
+            # Arrange parameters into an array and take the gradient in parameter space to see which change will decrease the average cost the most.
+            negativeGradients = -self.numericGradient(self.parameterSpaceCostField(gradientSpacing, data, labels), gradientSpacing)
+            paramToChange = np.argmax(negativeGradients)
+            # Change the parameter with the highest negative gradient
+            for layer in self.layers:
+                paramToChange -= len(layer.biases)
+                if (paramToChange < 0):
+                    paramToChange += len(layer.biases)
+                    layer.biases[paramToChange] += negativeGradients[paramToChange]*learningRate
+                    break
+                for i in range(len(layer.weights)):
+                    paramToChange -= len(layer.weights[i])
+                    if (paramToChange < 0):
+                        paramToChange += len(layer.weights[i])
+                        layer.weights[i][paramToChange] += negativeGradients[paramToChange]*learningRate
+                        break
 
     def cost(self, data, labels):
-        """A function which takes in labelled data and outputs the sum of squared errors on that for the current model."""
+        """A function which takes in labelled data and outputs the average squared error on that for the current model."""
         SSE = 0
         for x in range(len(data)):
-            SSE += (labels[x] - self.predict(data[x]))**2
+            SSE += np.sum((labels[x] - self.predict(data[x]))**2)
         return (SSE/len(data))
 
+    def parameterSpaceCostField(self, gradientSpacing, data, labels):
+        """Iterates through each parameter in the neural network and canges it be gradientSpacing then evaluates the cost on given data and labels.
+        
+        Returns an array with 3 columns, cost with parameter - gradientSpacing, cost with current value, and cost with parameter + gradientSpacing.
+        These rows are arranged bias of first layer, weights of first layer, then the same for second layer, etc."""
+        numParams = 0
+        for layer in self.layers:
+            numParams += len(layer.biases)
+            for row in layer.weights:
+                numParams += len(row)
+        field = np.zeros((numParams, 3))
+        
+        # Begin iterating through parameters and evaluating costs.
+        x = 0
+        for layer in self.layers:
+            for i in range(len(layer.biases)):
+                layer.biases[i] -= gradientSpacing
+                field[x][0] = self.cost(data, labels)
+                layer.biases[i] += gradientSpacing
+                field[x][1] = self.cost(data, labels)
+                layer.biases[i] += gradientSpacing
+                field[x][2] = self.cost(data, labels)
+                layer.biases[i] -= gradientSpacing
+                x += 1
+            for weightRow in layer.weights:
+                for i in range(len(weightRow)):
+                    weightRow[i] -= gradientSpacing
+                    field[x][0] = self.cost(data, labels)
+                    weightRow[i] += gradientSpacing
+                    field[x][1] = self.cost(data, labels)
+                    weightRow[i] += gradientSpacing
+                    field[x][2] = self.cost(data, labels)
+                    weightRow[i] -= gradientSpacing
+                    x += 1
+        return field
 
-    def ReLu(self, input):
-        """Puts the input through a Rectified Linear Unit. (converts each input element to 0.0 if negative.)"""
-        for x in range(len(input)):
-            if input[x] < 0:
-                input[x] = 0
+
+    def numericGradient(self, input, spacing):
+        """A basic numeric gradient calculator. Takes a 3 column input and a spacing scalar and outputs a single column representing the gradients of the input."""
+        gradients = np.zeros(len(input))
+        x = 0
+        for row in input:
+            gradients[x] = (((row[1] - row[0]) + (row[2] - row[1]))*0.5)/spacing
+            x += 1
+        return gradients
 
 
     class layer(object):
-        """A subclass which holds a layer of a neural network."""
+        """A subclass which can hold a single layer of a neural network."""
 
-        __init__(self, inputLen, outputLen):
+        def __init__(self, inputLen, outputLen):
             """Initializes a layer's weights and biases. (maybe should be random instead of zero?)"""
-            self.weights = np.zeros((inputLen, outputLen))
-            self.biases = np.zeros(inputLen)
+            self.weights = np.zeros((outputLen, inputLen))
+            self.biases = np.zeros(outputLen)
+
+        def ReLu(self, input):
+            """Puts the input through a Rectified Linear Unit. (converts each input element to 0.0 if negative.)"""
+            for x in range(len(input)):
+                if input[x] < 0:
+                    input[x] = 0
 
         def evaluate(self, input):
             """Takes an input to the layer and returns the ReLu(output of the layer)."""
-            return ReLu(self.weights @ input + self.biases)
+            output = self.weights @ input + self.biases
+            self.ReLu(output)
+            return output
 
 
     # relu(W1A1 + W2A2 + W3A3... + WnAn - bias)

Connect4AI/Connect4AI/main.py
@@ -1,37 +1,37 @@
 # Main: this is being used to test and run implemented classes.
 
-import tensorflow as tf
-from tensorflow import keras
+#import tensorflow as tf
+#from tensorflow import keras
 from Connect4Board import Connect4Board
 import numpy as np
 from dataGenerator import dataGenerator
 
 # Randomly generate 5000 games worth of data
-test = dataGenerator(5000)
+test = dataGenerator(20)
 test.produceRandomData()
 
 # Make a model with layers of neurons, 1 input layer which takes a board and playerID, one hidden layer which goes to 100 neurons,
 # then a final layer where it goes to 7 cells representing each column where a move can be made.
-model = keras.Sequential([
-    keras.Input(shape=(43,)),
-    keras.layers.Dense(43, activation=tf.nn.relu),
-    keras.layers.Dense(7, activation=tf.nn.softmax)
-    ])
+#model = keras.Sequential([
+#    keras.Input(shape=(43,)),
+#    keras.layers.Dense(43, activation=tf.nn.relu),
+#    keras.layers.Dense(7, activation=tf.nn.softmax)
+#    ])
 
 # Pick a loss function and optimizer that determines how the model learns. 
-model.compile(optimizer="Adam",
-              loss='MSE') # Use the Adam algorithm for better computation times and (possibly) better results than SGD
+#model.compile(optimizer="Adam",
+#              loss='MSE') # Use the Adam algorithm for better computation times and (possibly) better results than SGD
 
-# Reformat the data to something which the neural net can be trained on. There's some significant optimization to be done here with all the array copying.
+# Reformat the data to something which the neural net can be trained on.
 features = np.empty((len(test.data),43))
 for x in range(len(test.data)):
     features[x] = np.append(test.data[x][0].flatten(), test.data[x][2])
-labels = np.zeros(len(test.data))
+labels = np.zeros((len(test.data), 7))
 for x in range(len(test.data)):
-    labels[x] = test.data[x][1]
+    labels[x][test.data[x][1]] = 1.0
 
 # Train the model
-model.fit(features, labels)
+#model.fit(features, labels)
 
 # Make a test board and feed the model that to see how it reacts.
 input = np.array([0,1,2,0,0,0,0,
@@ -43,7 +43,17 @@ input = np.array([0,1,2,0,0,0,0,
 input = np.append(input, 1)
 input2 = np.empty((1,43))
 input2[0] = input
-a = model.predict(input2)
-print(a)
+#a = model.predict(input2)
+#print(a)
 
-# Currently since the model is being trained on entirely random moves it is equally likely to pick any column.
+# Currently since the model is being trained on entirely random moves it should come out equally likely to pick any column.
+
+
+
+from FromScratchNN import FromScratchNN as FSNN
+
+newAI = FSNN([43, 7])
+newAI.trainGradientDescent(features, labels, 0.1, 0.5, 20)
+print(newAI.predict(input))
+
+# Once again since the model is being trained on entirely random moves it should come out equally likely to pick any column.